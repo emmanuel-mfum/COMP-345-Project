7fc18328be32f5668cbc10892541a692de2337c0
Shagana <47454503+shamahen@users.noreply.github.com>
2020-10-03 16:16:55 -0400

    Create player.cpp

---
+++ Player/player.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include"Player.h"
+#include"Map.h"
+using namespace std;
+
+Player::Player(){
+    armies =0;
+}
+
+void Player::toDefend(){
+
+}
+void Player::toAttack(Player *pl){
+    
+}
+
