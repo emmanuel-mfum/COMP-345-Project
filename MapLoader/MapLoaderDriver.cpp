26c96b1d3cf69601f522d9ace0c3b9ed88321ef4
DavidNguyenHuu <37845640+DavidNguyenHuu@users.noreply.github.com>
2020-10-12 14:06:55 -0400

    Update MapLoaderDriver.cpp

--- MapLoader/MapLoaderDriver.cpp
+++ MapLoader/MapLoaderDriver.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
+#include <stdexcept>
+#include "MapLoader.h"
 using namespace std;
 
-int main(){
-	
+int main() {
+    try {
+        MapLoader loader("./MapLoader/maps/TestMap.txt");
+        cout << "Map was successfully parsed." << endl;
+    }
+    catch (invalid_argument ex) {
+        cout << ex.what() << endl;
+    }
 }
