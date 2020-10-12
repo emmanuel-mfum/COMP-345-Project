2ca4e9bd601f0c6bd8e031132c57a10172cf38a9
DavidNguyenHuu <37845640+DavidNguyenHuu@users.noreply.github.com>
2020-10-12 18:42:10 -0400

    Update MapLoader.cpp

--- MapLoader/MapLoader.cpp
+++ MapLoader/MapLoader.cpp
@@ -4,8 +4,6 @@
 #include <sstream>
 #include <fstream>
 
-
-
 using namespace std;
 
 //default constructor
@@ -32,8 +30,7 @@ Map MapLoader::load_map(string fName) {
 	// Load the continents.
 	while (getline(input_stream, line_read) && line_read != "[continents]") {}
 	while (getline(input_stream, line_read) && line_read != "") {
-		//set all continents
-         
+		//Set all continents      
 		 const std::string newContinentName = line.substr();
 		 map->addContinentByName(newContinentName);
 
@@ -42,20 +39,17 @@ Map MapLoader::load_map(string fName) {
 	// Load the countries
 	while (getline(input_stream, line_read) && line_read != "[countries]") {}
 	while (getline(input_stream, line_read)) {
-		//set all countries
-
+		//Set all countries
 		Territory* newCountry = new Territory(TerritoryType::Country, "newCountryName");
 		map->addContinentByName(continentName, newCountryName);
 		map->addEdgeByName(territoryName, territoryName);
 	}
-
-
 	// The input stream is restarted from the beginning.
 	input_stream.clear();
 	input_stream.seekg(0, input_stream.beg);
 
 
-	//Set the borders
+	//Set territories
 
 	return map;
 
