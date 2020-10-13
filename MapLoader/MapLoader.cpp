a612c6691df6b85cdc4f7e0e74e061df5abcede6
Paul Piggott <Flying-piggott@hotmail.com>
2020-10-12 21:01:39 -0400

    Rewrote mapLoader

--- MapLoader/MapLoader.cpp
+++ MapLoader/MapLoader.cpp
@@ -1,20 +1,22 @@
+#pragma once
+
 #include "MapLoader.h"
 #include "../Map/Map.h"
 #include <iostream>
 #include <sstream>
 #include <fstream>
-
+#include <vector>
 
 
 using namespace std;
 
 //default constructor
 MapLoader::MapLoader() {
-	newMap = new Map();
+	this->newMap = nullptr;
 }
 //parameterized constructor
 MapLoader::MapLoader(string fName) {
-	newMap = load_map(textFileName);
+	this->newMap = load_map(fName);
 }
 
 //destructor
@@ -23,41 +25,121 @@ MapLoader::~MapLoader() {
 }
 
 
-Map MapLoader::load_map(string fName) {
+Map* MapLoader::load_map(string fName) {
 	Map* map = nullptr;
 
 	std::ifstream input_stream(fName);
 	std::string line_read;
 
+
+	bool isContinents = false;
+	bool isCountries = false;
+	bool isBorders = false;
+
+	bool readSplitToken = false;
+	
+	vector<string>* continents = new vector<string>;
+	vector<string>* countries = new vector<string>;
+
+	string delimiter = " ";
+
 	// Load the continents.
-	while (getline(input_stream, line_read) && line_read != "[continents]") {}
-	while (getline(input_stream, line_read) && line_read != "") {
-		//set all continents
-         
-		 const std::string newContinentName = line.substr();
-		 map->addContinentByName(newContinentName);
-
-	}
-
-	// Load the countries
-	while (getline(input_stream, line_read) && line_read != "[countries]") {}
+	int counter = 0;
 	while (getline(input_stream, line_read)) {
-		//set all countries
+		string line = line_read;
+		readSplitToken = false;
 
-		Territory* newCountry = new Territory(TerritoryType::Country, "newCountryName");
-		map->addContinentByName(continentName, newCountryName);
-		map->addEdgeByName(territoryName, territoryName);
+		// assume continents comes first, countries comes after, borders come after
+		if (counter == 0) {
+			// create a new map with the name
+			string mapName = line.substr(line.find("map: ") + 5);
+			map = new Map(mapName);
+		}
+		else if (line.compare("[continents]") == 0) {
+			// have to start reading continents
+			isContinents = true;
+			readSplitToken = true;
+		}
+		else if (line.compare("[countries]") == 0) {
+			// connect all the continents once we have finished reading them all
+			for (int i = 0; i < continents->size() - 1; i++) {
+				map->addEdgeByName(continents->at(i), continents->at(i + 1));
+			}
+			// have to start reading countries
+			isContinents = false;
+			isCountries = true;
+			readSplitToken = true;
+		}
+		else if (line.compare("[borders]") == 0) {
+			// have to start reading the borders
+			isCountries = false;
+			isBorders = true;
+			readSplitToken = true;
+		}
+
+		if (!readSplitToken && line.compare("") != 0) {
+			// have to read the component on the line
+			if (isContinents) {
+				// add the continent to the map
+				string continentName = line.substr(0, line.find(" "));
+				cout << "Created territory: " + continentName << endl;
+				map->addContinentByName(continentName);
+				continents->push_back(continentName);
+			}
+			else if (isCountries) {
+				// add the country to the map
+				size_t pos = 0;
+				string token;
+				int countryCounter = 0;
+				string countryName;
+				// tokenize the line
+				while ((pos = line.find(delimiter)) != string::npos) {
+					token = line.substr(0, pos);
+					// found the country name
+					if (countryCounter == 1) {
+						countryName = token;
+					}
+					// now know which continent the country belongs to, can be added to map
+					else if (countryCounter == 2) {
+						string continentName = continents->at(stoi(token) - 1);
+						map->addCountryByName(continentName, countryName);
+						countries->push_back(countryName);
+					}
+
+					line.erase(0, pos + delimiter.length());
+					countryCounter++;
+				}
+			}
+			else if (isBorders) {
+				// add the border to the map
+				size_t pos = 0;
+				string token;
+				string countryName;
+				int borderCounter = 0;
+				// tokenize the string
+				while ((pos = line.find(delimiter)) != string::npos) {
+					token = line.substr(0, pos);
+					// found which country the borders are for
+					if (borderCounter == 0) {
+						countryName = countries->at(stoi(token) - 1);
+					}
+
+					else {
+						// add an edge
+						map->addEdgeByName(countryName, countries->at(stoi(token) - 1));
+					}
+
+					line.erase(0, pos + delimiter.length());
+					borderCounter++;
+				}
+			}
+		}
+		counter++;
 	}
 
-
-	// The input stream is restarted from the beginning.
 	input_stream.clear();
 	input_stream.seekg(0, input_stream.beg);
 
-
-	//Set the borders
-
 	return map;
-
 }
 
