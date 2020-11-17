a2fdd55bb858f5f775362d3c60477d35dd8795c2
DavidNguyenHuu <37845640+DavidNguyenHuu@users.noreply.github.com>
2020-11-16 21:37:54 -0500

    Update GameEngine.h

---
+++ GameEngine/GameEngine.h
@@ -0,0 +1,35 @@
+#include "Map.h"
+#include "MapLoader.h"
+#include "Player.h"
+#include "Cards.h"
+
+#include <list>
+
+
+class Player;
+class Deck;
+class Map;
+class Continent;
+
+
+class GameEngine
+{
+private :
+
+	list <Player> playerList;
+	Player* Player;
+	Map* map;
+	Deck* deck;
+
+public:
+	
+	static const char* directory;
+
+	GameEngine();
+	~GameEngine();
+
+	static void gameStart();
+	static string mapSelection();
+	static int playerSelection();
+
+};
