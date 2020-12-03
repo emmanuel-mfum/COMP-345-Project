8d24c6ae0f91a170a2c1c8b5b2fad5f8562824cd
Emmanuel <55893421+emmanuel-mfum@users.noreply.github.com>
2020-12-02 23:04:01 -0500

    Added more attributes, methods
    Added an assignment operator for the Adapter as well as a string attribute for the MapLoader with a getter

--- MapLoader/MapLoader.h
+++ MapLoader/MapLoader.h
@@ -11,24 +11,28 @@ class Map;
 
 class MapLoader {
 
-private: 
+private:
 	Map* newMap;
+	string filename;
 
 public:
+	MapLoader();
 	MapLoader(string fName);
 	~MapLoader();
 	static Map* load_map(std::string fName);
-}
+	string getFileName();
+};
 
 class ConquestFileReader
 {
 private:
-	Map* newConquestMap
+	Map* newConquestMap;
 public:
+	ConquestFileReader();
 	ConquestFileReader(string fName);
 	~ConquestFileReader();
 	static Map* load_ConquestMap(std::string fName);
-}
+};
 
 
 
@@ -38,14 +42,20 @@ private:
 	MapLoader* domination;
 	ConquestFileReader* conquest;
 	bool isDomination;
+	string fileName;
 
 public:
+	ConquestFileReaderAdapter();
 	ConquestFileReaderAdapter(MapLoader* maploader);
 	ConquestFileReaderAdapter(ConquestFileReader* conquest);
 	ConquestFileReaderAdapter(ConquestFileReaderAdapter& mapReader);
 	~ConquestFileReaderAdapter();
+	friend ostream& operator<<(ostream& out, const ConquestFileReaderAdapter& mapL); // stream operator
+	ConquestFileReaderAdapter& operator=(const ConquestFileReaderAdapter& mapL); // assignment operator
 	void setBool(int input);
 	Map* parseMap(string name);
+	string getFileName();
+	void setFileName(string fileName);
 
 
 };
